3f97b2279a61dcfe501b7f0d9cf27af9f1bcaa6a
Ricardo Carriel <ric.carriel@hotmail.com>
2019-05-12 01:54:46 -0300

    Mensagem acrescentada

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace Teste {
     class Program {
         static void Main(string[] args) {
-            
+            Console.WriteLine("Dale");
         }
     }
 }
